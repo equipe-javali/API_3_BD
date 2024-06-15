f66d5d1e6ddab4576a2224fd7c0fb1ba49900961
mvbetti <73323386+marcusvbe@users.noreply.github.com>
2024-06-15 20:14:47 -0300

    GERAL - Atualizacao tabela AtivoTangivel.sql

--- sql/tables/AtivoTangivel.sql
+++ sql/tables/AtivoTangivel.sql
@@ -1,6 +1,9 @@
-CREATE TABLE AtivoTangivel (
-    id_ativo SERIAL PRIMARY KEY,
+CREATE TABLE ativotangivel (
+    id_ativo BIGINT NOT NULL,
+    marca VARCHAR(100),
     garantia DATE,
-    taxa_depreciacao NUMERIC(2),
-    periodo_depreciacao VARCHAR(30)
+    taxa_depreciacao NUMERIC(10, 2),
+    periodo_depreciacao VARCHAR(30),
+    CONSTRAINT ativotangivel_pkey PRIMARY KEY (id_ativo),
+    CONSTRAINT ativotangivel_id_ativo_fkey FOREIGN KEY (id_ativo) REFERENCES ativo (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
