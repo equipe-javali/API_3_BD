125b3523c207d864a240ce269da9f47d59fc2e59
mvbetti <73323386+marcusvbe@users.noreply.github.com>
2024-06-15 20:35:31 -0300

    GERAL - Atualizacao tabela Usuario.sql

--- sql/tables/Usuario.sql
+++ sql/tables/Usuario.sql
@@ -1,11 +1,15 @@
-CREATE TABLE Usuario (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE usuario (
+    id BIGSERIAL,
     nome VARCHAR(100),
     cpf VARCHAR(11),
     nascimento DATE,
     departamento VARCHAR(20),
     telefone VARCHAR(20),
     email VARCHAR(100),
-    perfil smallint,
-    status VARCHAR(100)
+    status VARCHAR,
+    perfil SMALLINT,
+    tipo_foto VARCHAR(30),
+    dados_foto BYTEA,
+    CONSTRAINT usuario_pkey PRIMARY KEY (id),
+    CONSTRAINT usuario_tipo_foto_check CHECK ((LENGTH(tipo_foto) <= 30))
 );
