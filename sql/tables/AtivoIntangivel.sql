947af47de039597ecd0a4e6b3a735dc580ef2639
mvbetti <73323386+marcusvbe@users.noreply.github.com>
2024-06-15 20:11:46 -0300

    GERAL - Atualizacao tabela AtivoIntangivel.sql

--- sql/tables/AtivoIntangivel.sql
+++ sql/tables/AtivoIntangivel.sql
@@ -1,6 +1,9 @@
-CREATE TABLE AtivoIntangivel (
-    id_ativo SERIAL PRIMARY KEY,
+CREATE TABLE ativointangivel (
+    id_ativo BIGINT NOT NULL,
     data_expiracao DATE,
-    taxa_amortizacao NUMERIC(2),
-    periodo_amortizacao VARCHAR(30)
+    taxa_amortizacao NUMERIC(10, 2),
+    periodo_amortizacao VARCHAR(30),
+    CONSTRAINT ativointangivel_pkey PRIMARY KEY (id_ativo),
+    CONSTRAINT ativointangivel_id_ativo_key UNIQUE (id_ativo),
+    CONSTRAINT ativointangivel_id_ativo_fkey FOREIGN KEY (id_ativo) REFERENCES ativo (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
