53cadd5b6d91391a4d11dba38f5297db91b3ef7a
mvbetti <73323386+marcusvbe@users.noreply.github.com>
2024-06-15 20:38:00 -0300

    GERAL - Atualizacao tabela UsuarioLogin.sql

--- sql/tables/UsuarioLogin.sql
+++ sql/tables/UsuarioLogin.sql
@@ -1,5 +1,7 @@
-CREATE TABLE UsuarioLogin (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE usuariologin (
+    id BIGINT NOT NULL,
     senha VARCHAR(60) NOT NULL,
-    FOREIGN KEY (id) REFERENCES Usuario(id)
+    CONSTRAINT usuariologin_pkey PRIMARY KEY (id),
+    CONSTRAINT usuariologin_id_key UNIQUE (id),
+    CONSTRAINT usuariologin_id_fkey FOREIGN KEY (id) REFERENCES usuario (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
